d6f07663f3ade8ac6ff58ca4277f07d1a4fb00ea
rendahl.weishar <rendahl.weishar@U-3AUDKSUNKHQL2.corp.classwallet.com>
2023-11-20 11:26:55 -0800

    Updated package script

--- package_cw_extension.sh
+++ package_cw_extension.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+VERSION=beta2.12
 # Check for syntax errors
 find . -type f -name *.php | xargs -n 1 php -l && \
-zip -r Magento_ClassWallet-beta2.11.zip ClassWallet
+zip -r Magento_ClassWallet-$VERSION.zip ClassWallet && \
+md5sum Magento_ClassWallet-$VERSION.zip
